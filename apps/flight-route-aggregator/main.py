83b7166c055e2f50168b30585ffd0c1c5c1ea41a
Ibrahim Abdelkareem <ibrahim.abdelkareem.1990@gmail.com>
2022-10-31 01:01:12 +0100

    feat: Spark + Kafka Integration

--- apps/flight-route-aggregator/main.py
+++ apps/flight-route-aggregator/main.py
@@ -1,30 +1,14 @@
-import sys
-import os
 from geopy.geocoders import Nominatim
 from pyspark.conf import SparkConf
-from pyspark.sql import SparkSession, Catalog
-from pyspark.sql import DataFrame, DataFrameStatFunctions, DataFrameNaFunctions
-from pyspark.sql import functions as F
-from pyspark.sql import types as T
-from pyspark.sql.types import Row
-from operator import add
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col, udf
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType
 
-spark_conf = SparkConf()
-spark_conf.setAll([
-    ('spark.master', 'spark://spark-master:7077'),
-    ('spark.app.name', 'TestApp'),
-    # ('spark.submit.deployMode', 'client'),
-    # ('spark.ui.showConsoleProgress', 'true'),
-    # ('spark.eventLog.enabled', 'false'),
-    # ('spark.logConf', 'false'),
-    # ('spark.driver.bindAddress', 'vps00'),
-    # ('spark.driver.host', 'localhost'),
-])
+print("Starting Consumer")
 
-spark = SparkSession.builder.config(conf=spark_conf).getOrCreate()
-sc = spark.sparkContext
+def get_country(lat, lon):
+    return "SE"
 
-# Reverse geolocator function (input: (lat, lon) | output: country name)    
 def locator(lat, lon):
     try:
         geolocator = Nominatim(user_agent="geoapiExercises")
@@ -34,13 +18,60 @@ def locator(lat, lon):
     except:
         return "International Waters"
 
-data = sc.parallelize(list("Hello World"))
-counts = data.map(lambda x:
-                  (x, 1)).reduceByKey(add).sortBy(lambda x: x[1],
-                                                  ascending=False).collect()
+schema = StructType(
+    [
+        StructField("Tag", StringType()),
+        StructField("To", StringType()),
+        StructField("From", StringType()),
+        StructField("Timestamp", StringType()),
+        StructField("Lat", DoubleType()),
+        StructField("Lon", DoubleType()),
+        StructField("Alt", DoubleType())
+    ]
+)
 
-for (word, count) in counts:
-    print("{}: {}".format(word, count))
+spark_conf = SparkConf().setAppName(
+    "flight-route-aggregator").setMaster("spark://spark-master:7077")
 
-spark.stop()
-quit()
+spark = SparkSession.builder.config(conf=spark_conf).getOrCreate()
+spark.sparkContext.setLogLevel("WARN")
+
+df = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "kafka:29092") \
+    .option("subscribe", "live_flight_traffic") \
+    .load()
+
+get_country_udf = udf(lambda lat, lon: get_country(lat, lon), StringType())
+
+query = df.select(from_json(col("value").cast("string"), schema).alias("parsed_value")) \
+    .select(col("parsed_value.*")) \
+    .withColumn("country", get_country_udf(col("lat"), col("lon"))) \
+    .writeStream \
+    .outputMode("Append") \
+    .format("console") \
+    .start() \
+
+# .select(col("parsed_value.*")) \
+# query = df.select(from_json(col("value").cast("string").alias("parsed_value"), schema)) \
+#     .writestream \
+#     .outputmode("append") \
+#     .format("console") \
+#     .start() \
+
+query.awaitTermination()
+
+
+# Reverse geolocator function (input: (lat, lon) | output: country name)
+
+# data = sc.parallelize(list("Hello World"))
+# counts = data.map(lambda x:
+#                   (x, 1)).reduceByKey(add).sortBy(lambda x: x[1],
+#                                                   ascending=False).collect()
+
+# for (word, count) in counts:
+#     print("{}: {}".format(word, count))
+
+# spark.stop()
+# quit()
