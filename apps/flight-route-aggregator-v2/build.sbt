5f61d30ae3f7ff8e4be25c30d65523475caf12c7
Ibrahim Abdelkareem <ibrahim.abdelkareem.1990@gmail.com>
2022-11-01 22:06:26 +0100

    feat: Adding Country

--- apps/flight-route-aggregator-v2/build.sbt
+++ apps/flight-route-aggregator-v2/build.sbt
@@ -1,6 +1,9 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
-
 ThisBuild / scalaVersion := "2.12.15"
+
+Compile / packageBin / mappings += {
+  (baseDirectory.value / "src" / "main" / "scala" / "cities1000.txt") -> "cities1000.txt"
+}
 
 val sparkVersion = "3.3.0"
 
@@ -8,8 +11,18 @@ libraryDependencies += "org.apache.spark" %% "spark-sql" % sparkVersion % "provi
 libraryDependencies += "org.apache.spark" %% "spark-sql-kafka-0-10" % sparkVersion % "provided"
 libraryDependencies += "org.apache.spark" %% "spark-streaming" % sparkVersion % "provided"
 libraryDependencies += "org.mongodb.spark" % "mongo-spark-connector" % "10.0.4" % "provided"
-libraryDependencies += "io.github.coordinates2country" % "coordinates2country" % "1.2"
+/* libraryDependencies += "io.github.coordinates2country" % "coordinates2country" % "1.2" */
+// libraryDependencies += "com.github.Vineeth-Mohan" % "OfflineReverseGeocode" % "1.0"
+
 lazy val root = (project in file("."))
   .settings(
     name := "flight-route-aggregator-v2"
   )
+
+//resolvers += "jitpack" at "https://jitpack.io"
+resourceDirectory in Compile := baseDirectory.value / "src" / "main" / "data"
+
+artifactName := { (sv: ScalaVersion, module: ModuleID, artifact: Artifact) =>
+  "flight-route-aggregator.jar"
+}
+
--- apps/flight-route-aggregator-v2/src/main/scala/Main.scala
+++ apps/flight-route-aggregator-v2/src/main/scala/Main.scala
@@ -7,6 +7,11 @@ import org.apache.spark.sql.types.{
   StructType
 };
 import com.mongodb.spark.sql.connector.config.MongoConfig
+import org.apache.spark.sql.streaming.DataStreamWriter
+import org.apache.spark.sql.streaming.StreamingQuery
+import com.mongodb.spark.sql.connector.config.WriteConfig
+import geocode.ReverseGeoCode
+import java.io.FileInputStream
 
 case class FlightRoute(
     Tag: String,
@@ -19,7 +24,20 @@ case class FlightRoute(
     Country: String
 );
 
+case class FlightRouteAggreagted(_id: String, Country: String, Count: Long)
+
 object Main {
+
+  var stream = getClass().getResourceAsStream("cities1000.txt")
+  var geoCoder = new ReverseGeoCode(stream, true)
+
+  def get_country(lat: Double, lon: Double): String = {
+    // val country =
+    //   io.github.coordinates2country.Coordinates2Country.country(lat, lon);
+
+    var place = geoCoder.nearestPlace(lat, lon)
+    return if (place != null) place.country else "International Water"
+  }
 
   def main(args: Array[String]): Unit = {
     val schema = StructType(
@@ -44,8 +62,10 @@ object Main {
 
     spark.sparkContext.setLogLevel("WARN")
 
-    val get_country = (lat: Double, lon: Double) => "SE"
-    val get_country_udf = spark.udf.register("get_country", get_country)
+    val get_country_udf = spark.udf.register(
+      "get_country",
+      (lat: Double, lon: Double) => get_country(lat, lon)
+    )
 
     val query = spark.readStream
       .format("kafka")
@@ -63,39 +83,45 @@ object Main {
       .distinct()
       .groupByKey(o => o._2)
       .count()
-    // .map(o => (o.Tag, o.Country))
-    // .distinct()
-    // .groupByKey(o => o._2)
-    // .count()
-    // .as("Count")
+      .as("count")
 
-    //    query
-    //      .writeStream
-    //      .format("console")
-    //      .outputMode("update")
-    //      .start()
-    //      .awaitTermination()
-    //
-    //
-
-    // import scala.collection.JavaConverters._;
-    // var map = Map[String, String]("first" -> "QQ").asJava
-    // var mongoConfig = MongoConfig.writeConfig(map).toWriteConfig().getClass()
-
-    query.writeStream
-      .format("mongodb")
-      .option(
-        "spark.mongodb.connection.uri",
-        "mongodb+srv://flight-route-publisher:WGfvPkzfyNL31grO@flightdatacluster.neyieqx.mongodb.net/flights.flight-aggregated-data?retryWrites=true&w=majority"
-      )
-      .option("spark.mongodb.database", "flights")
-      .option("spark.mongodb.collection", "flight-aggregated-data")
-      .option("spark.mongodb.change.stream.publish.full.document.only", "true")
+    val writer = query.writeStream
       .option("checkpointLocation", "/opt/spark/checkpoint")
       .option("forceDeleteTempCheckpointLocation", "true")
-      .outputMode("complete")
-      .start()
-      .awaitTermination()
+      .outputMode("update")
 
+    UseConsole(writer).awaitTermination()
+  }
+
+  def UseMongo[T](writer: DataStreamWriter[T]): StreamingQuery = {
+
+    import scala.collection.JavaConverters._;
+    var config = Map[String, String](
+      "connection.uri" -> "mongodb+srv://flight-route-publisher:WGfvPkzfyNL31grO@flightdatacluster.neyieqx.mongodb.net/flights.flight-aggregated-data?retryWrites=true&w=majority",
+      "database" -> "flights",
+      "collection" -> "flight-aggregated-data",
+      "change.stream.publish.full.document.only" -> "true"
+    ).asJava
+
+    var mongoConfig = MongoConfig.writeConfig(config).getOptions().asScala
+
+    return writer
+      .format("mongodb")
+      .options(mongoConfig)
+      // .option(
+      //   "spark.mongodb.connection.uri",
+      //   "mongodb+srv://flight-route-publisher:WGfvPkzfyNL31grO@flightdatacluster.neyieqx.mongodb.net/flights.flight-aggregated-data?retryWrites=true&w=majority"
+      // )
+      // .option("spark.mongodb.database", "flights")
+      // .option("spark.mongodb.collection", "flight-aggregated-data")
+      // .option("spark.mongodb.change.stream.publish.full.document.only", "true")
+      .start()
+  }
+
+  def UseConsole[T](writer: DataStreamWriter[T]): StreamingQuery = {
+
+    return writer
+      .format("console")
+      .start()
   }
 }
